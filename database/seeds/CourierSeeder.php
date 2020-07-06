7bfc1b99b0f0a9f3ed38f17c871c7b28d716eedd
IDStack <idstack@IDStacks-MacBook-Pro.local>
2020-07-06 16:26:04 +0700

    persiapan model dan migration project

---
+++ database/seeds/CourierSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CourierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
