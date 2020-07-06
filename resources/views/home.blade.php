ceddf90aff0acb41bbcb8b60d69eb86e7c599dd5
IDStack <idstack@IDStacks-MacBook-Pro.local>
2020-07-06 17:18:50 +0700

    membuat halaman homepage cek ongkir

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,101 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="ongkir-header">
+        <h1>CodeOngkir</h1>
+        <p class="lead">
+            Project Cek Ongkir ke Seluruh Koda dan Kabupaten di Indonesia
+        </p>
+    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Free</h4>
+            </div>
+            <div class="card-body">
+                <i class="fas fa-truck" style="font-size:80px"></i>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Cek Ongkir Lebih Mudah</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+            </div>
+        </div>
 
-                    {{ __('You are logged in!') }}
-                </div>
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Pro</h4>
+            </div>
+            <div class="card-body">
+                <i class="fas fa-box" style="font-size:80px"></i>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Lacak lokasi paket</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+            </div>
+        </div>
+
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Enterprise</h4>
+            </div>
+            <div class="card-body">
+                <i class="fas fa-plane-departure" style="font-size:80px"></i>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Cek Ongkir Pengiriman Internasional</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+    <div class="card">
+        <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Formulir Cek Ongkir</h4>
+        </div>
+        <div class="card-body">
+            <form action="">
+                <div class="form-row">
+                    <div class="col">
+                        <h5 class="text-muted">Asal Pengirim:</h5>
+                        <div class="form-group">
+                            <label for="">Provinsi</label>
+                            <select name="origin_province" id="" class="form-control">
+                                <option value="#">-</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Kota/Kabupaten</label>
+                            <select name="origin_city" id="" class="form-control">
+                                <option value="#">-</option>
+                            </select>
+                        </div>
+                        <h5 class="text-muted">Tujuan Pengirim:</h5>
+                        <div class="form-group">
+                            <label for="">Kota/Kabupaten</label>
+                            <select name="destination_city" id="" class="form-control">
+                                <option value="#">-</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <h5 class="text-muted">Pilih Expedisi:</h5>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox1">JNE</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                            <label class="form-check-label" for="inlineCheckbox2">TIKI</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/js/all.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
@@ -18,6 +19,12 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .ongkir-header {
+            padding: 3rem 1.5rem;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
